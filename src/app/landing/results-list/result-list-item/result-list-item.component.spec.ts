2f9942b06c1a39c44f438d3e27d7371cb5c65eb9
Carlos Hernández Gil <carlos.hernandez@mediktiv.com>
2020-11-19 13:13:57 +0100

    Unit test for result-list-item component

--- src/app/landing/results-list/result-list-item/result-list-item.component.spec.ts
+++ src/app/landing/results-list/result-list-item/result-list-item.component.spec.ts
@@ -1,25 +1,39 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ResultListItemComponent } from './result-list-item.component';
 
-describe('ResultListItemComponent', () => {
+fdescribe('ResultListItemComponent', () => {
   let component: ResultListItemComponent;
   let fixture: ComponentFixture<ResultListItemComponent>;
-
+  let router: Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResultListItemComponent ]
+      declarations: [ ResultListItemComponent ],
+      imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(ResultListItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.result = {url: 'url', name: 'name'}
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('[gotoDetails()] should go to details view', ()=> {
+    spyOn(router, 'navigate');
+    component.result = { url: 'url', name: 'picachu' };
+    component.gotoDetails();
+    expect(router.navigate).toHaveBeenCalledWith(['/pokemon' , 'picachu'])
   });
 });
