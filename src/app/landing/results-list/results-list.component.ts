fc97b41f7228f1b16e0e062b64a430e4fade3458
Carlos Hernández Gil <carlos.hernandez@mediktiv.com>
2020-11-19 11:54:20 +0100

    Add results list component

---
+++ src/app/landing/results-list/results-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// models
+import { ResponseItem } from 'src/app/models/responseItem';
+
+class Props {
+  results: ResponseItem[] = []
+}
+
+@Component({
+  selector: 'app-results-list',
+  templateUrl: './results-list.component.html',
+  styleUrls: ['./results-list.component.scss'],
+})
+export class ResultsListComponent implements OnInit {
+  state: Props = new Props();
+  @Input() results: ResponseItem[];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.state.results = this.results.map((result) => {
+      return {
+        name: result.name,
+        url: `https://img.pokemondb.net/sprites/black-white/anim/normal/${result.name}.gif`,
+      };
+    });
+  }
+}
