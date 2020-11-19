e67fabfd000f769e98521b5e53bb6cf3763018e1
Carlos Hernández Gil <carlos.hernandez@mediktiv.com>
2020-11-19 13:39:28 +0100

    Add spec loader component

--- src/app/loader/loader.component.spec.ts
+++ src/app/loader/loader.component.spec.ts
@@ -1,14 +1,16 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoaderComponent } from './loader.component';
 
-describe('LoaderComponent', () => {
+fdescribe('LoaderComponent', () => {
   let component: LoaderComponent;
   let fixture: ComponentFixture<LoaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoaderComponent ]
+      declarations: [ LoaderComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -16,10 +18,10 @@ describe('LoaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
