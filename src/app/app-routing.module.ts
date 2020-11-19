886793da8a01d65ff11947685e1f13b31876a145
Carlos Hernández Gil <carlos.hernandez@mediktiv.com>
2020-11-19 13:39:54 +0100

    Add app and unit tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/pokemon' },
+  {
+    path: 'pokemon/:id',
+    loadChildren: () =>
+      import('../app/pokemon-details/pokemon-details.module').then(
+        (m) => m.PokemonDetailsModule
+      ),
+  },
+  { path: 'pokemon', component: LandingComponent },
+  { path: '**', redirectTo:'/pokemon'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -26,10 +28,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('code-challenge-pokemon');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('code-challenge-pokemon app is running!');
-  });
+
 });
